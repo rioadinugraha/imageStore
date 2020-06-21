015dce330f5f0b56f13ff95108fbf9b818a13f85
Rio Nugraha <43334190+rioadinugraha@users.noreply.github.com>
2020-06-21 23:25:43 +0700

    IMAGE WEBSERVICE

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://admin1:commservicer1@database-1.c02risgqtnhz.ap-southeast-1.rds.amazonaws.com:3306/app3'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:''@localhost/app3'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin1:commservicer1@database-1.c02risgqtnhz.ap-southeast-1.rds.amazonaws.com:3306/app3'
     FLASK_RUN_PORT = 5001
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
